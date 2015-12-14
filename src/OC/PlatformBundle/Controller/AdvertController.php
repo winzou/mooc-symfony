300f07e0223a872b9e6b10dc0e5f97dcccf19367
Alexandre Bacco <alexandre.bacco@gmail.com>
2015-12-14 15:30:09 +0800

    Iteration 3 - URL generation

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -12,12 +12,14 @@ class AdvertController extends Controller
 {
   public function indexAction()
   {
-    $content = $this
-      ->get('templating')
-      ->render('OCPlatformBundle:Advert:index.html.twig', array('nom' => 'winzou'))
-    ;
+    // On veut avoir l'URL de l'annonce d'id 5.
+    $url = $this->get('router')->generate(
+      'oc_platform_view', // 1er argument : le nom de la route
+      array('id' => 5)    // 2e argument : les valeurs des paramètres
+    );
+    // $url vaut « /platform/advert/5 »
 
-    return new Response($content);
+    return new Response("L'URL de l'annonce d'id 5 est : ".$url);
   }
 
   // La route fait appel à OCPlatformBundle:Advert:view, on doit donc définir la méthode viewAction.
