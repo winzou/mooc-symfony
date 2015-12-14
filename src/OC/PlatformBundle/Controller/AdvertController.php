d816d35624c66683ae3e5753461079f071b2c3d8
Alexandre Bacco <alexandre.bacco@gmail.com>
2015-12-14 15:28:24 +0800

    Iteration 3 - routes and controller actions

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -19,4 +19,22 @@ class AdvertController extends Controller
 
     return new Response($content);
   }
+
+  // La route fait appel à OCPlatformBundle:Advert:view, on doit donc définir la méthode viewAction.
+  // On donne à cette méthode l'argument $id, pour correspondre au paramètre {id} de la route
+  public function viewAction($id)
+  {
+    // $id vaut 5 si l'on a appelé l'URL /platform/advert/5
+
+    // Ici, on récupèrera depuis la base de données l'annonce correspondant à l'id $id.
+    // Puis on passera l'annonce à la vue pour qu'elle puisse l'afficher
+
+    return new Response("Affichage de l'annonce d'id : ".$id);
+  }
+
+  // On récupère tous les paramètres en arguments de la méthode
+  public function viewSlugAction($slug, $year, $_format)
+  {
+    return new Response("On pourrait afficher l'annonce correspondant au slug '".$slug."', créée en ".$year." et au format ".$_format.".");
+  }
 }
