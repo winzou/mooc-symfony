e572dc4fba09eb661aef3cce22426be6ea3380a0
Alexandre Bacco <alexandre.bacco@gmail.com>
2016-02-09 22:26:23 +0800

    Iteration 14 - Add image upload

--- src/OC/PlatformBundle/Entity/Advert.php
+++ src/OC/PlatformBundle/Entity/Advert.php
@@ -57,7 +57,7 @@ class Advert
   private $published = true;
 
   /**
-   * @ORM\OneToOne(targetEntity="OC\PlatformBundle\Entity\Image", cascade={"persist"})
+   * @ORM\OneToOne(targetEntity="OC\PlatformBundle\Entity\Image", cascade={"persist", "remove"})
    */
   private $image;
 
--- src/OC/PlatformBundle/Form/ImageType.php
+++ src/OC/PlatformBundle/Form/ImageType.php
@@ -4,7 +4,7 @@
 namespace OC\PlatformBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,8 +13,8 @@ class ImageType extends AbstractType
   public function buildForm(FormBuilderInterface $builder, array $options)
   {
     $builder
-      ->add('url', TextType::class)
-      ->add('alt', TextType::class);
+      ->add('file', FileType::class)
+    ;
   }
 
   public function configureOptions(OptionsResolver $resolver)
