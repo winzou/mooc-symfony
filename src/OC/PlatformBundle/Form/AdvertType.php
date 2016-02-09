dd8ed3b890a82932b0ce14e1fda4526358c505d8
Alexandre Bacco <alexandre.bacco@gmail.com>
2016-02-09 21:59:48 +0800

    Iteration 14 - Use form events to change the form

--- src/OC/PlatformBundle/Form/AdvertType.php
+++ src/OC/PlatformBundle/Form/AdvertType.php
@@ -12,6 +12,8 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class AdvertType extends AbstractType
@@ -26,7 +28,6 @@ class AdvertType extends AbstractType
       ->add('title',     TextType::class)
       ->add('author',    TextType::class)
       ->add('content',   TextareaType::class)
-      ->add('published', CheckboxType::class, array('required' => false))
       ->add('image',     ImageType::class)
       ->add('categories', EntityType::class, array(
         'class'         => 'OCPlatformBundle:Category',
@@ -36,7 +37,31 @@ class AdvertType extends AbstractType
           return $repository->getLikeQueryBuilder($pattern);
         }
       ))
-      ->add('save',      SubmitType::class);
+      ->add('save',      SubmitType::class)
+    ;
+
+    // On ajoute une fonction qui va écouter un évènement
+    $builder->addEventListener(
+      FormEvents::PRE_SET_DATA,    // 1er argument : L'évènement qui nous intéresse : ici, PRE_SET_DATA
+      function(FormEvent $event) { // 2e argument : La fonction à exécuter lorsque l'évènement est déclenché
+        // On récupère notre objet Advert sous-jacent
+        $advert = $event->getData();
+
+        // Cette condition est importante, on en reparle plus loin
+        if (null === $advert) {
+          return; // On sort de la fonction sans rien faire lorsque $advert vaut null
+        }
+
+        // Si l'annonce n'est pas publiée, ou si elle n'existe pas encore en base (id est null)
+        if (!$advert->getPublished() || null === $advert->getId()) {
+          // Alors on ajoute le champ published
+          $event->getForm()->add('published', CheckboxType::class, array('required' => false));
+        } else {
+          // Sinon, on le supprime
+          $event->getForm()->remove('published');
+        }
+      }
+    );
   }
 
   public function configureOptions(OptionsResolver $resolver)
