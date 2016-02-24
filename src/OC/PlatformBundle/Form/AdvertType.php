ac8c2b26a57c4b3ce93fa4f0be02b797f1748e32
Alexandre Bacco <alexandre.bacco@gmail.com>
2016-02-24 18:40:39 +0100

    Iteration 17 - Use tags to create custom form field

--- src/OC/PlatformBundle/Form/AdvertType.php
+++ src/OC/PlatformBundle/Form/AdvertType.php
@@ -27,7 +27,7 @@ class AdvertType extends AbstractType
       ->add('date',      DateTimeType::class)
       ->add('title',     TextType::class)
       ->add('author',    TextType::class)
-      ->add('content',   TextareaType::class)
+      ->add('content',   CkeditorType::class)
       ->add('image',     ImageType::class)
       ->add('categories', EntityType::class, array(
         'class'         => 'OCPlatformBundle:Category',
