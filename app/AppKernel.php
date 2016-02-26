51e7ba94061605bd85d9f2b34f4f999ef2ed4b81
Alexandre Bacco <alexandre.bacco@gmail.com>
2016-02-26 17:51:15 +0100

    Iteration 23 - Install a console bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -30,6 +30,7 @@ class AppKernel extends Kernel
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
             $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
+            $bundles[] = new CoreSphere\ConsoleBundle\CoreSphereConsoleBundle();
         }
 
         return $bundles;
